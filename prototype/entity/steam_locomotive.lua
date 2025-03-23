5d5805e1e08d5b062864ca59c865502e2e5013b8
IonShieldQuad <IonShieldQuad@users.noreply.github.com>
2025-03-24 01:06:05 +0300

    fix 1

--- prototype/entity/steam_locomotive.lua
+++ prototype/entity/steam_locomotive.lua
@@ -18,27 +18,27 @@ local custom_properties = {
 	connection_distance = 3,
 	energy_source = {
 		type = "burner",
-		emissions_per_minute = {pollution = 10},
+		emissions_per_minute = { pollution = 10 },
 		fuel_inventory_size = 3,
 		render_no_network_icon = false,
 		smoke = {
-		  {
-			deviation = {0.1,0.1},
-			frequency = 100,
-			name = "rtc-train-smoke",
-			position = {0,-2.1},
-			height = 1.7,
-			starting_vertical_speed = 0.15,
-			starting_vertical_speed_deviation = 0.1,
-		  }
+			{
+				deviation = { 0.1, 0.1 },
+				frequency = 100,
+				name = "rtc-train-smoke",
+				position = { 0, -2.1 },
+				height = 1.7,
+				starting_vertical_speed = 0.15,
+				starting_vertical_speed_deviation = 0.1,
+			}
 		}
 	},
 	working_sound = {
 		sound = {
-			filename = SOUND_PATH.."steam-engine-1.ogg",
+			filename = SOUND_PATH .. "steam-engine-1.ogg",
 			volume = 0.2,
 			speed = 0.1,
-			min_speed = 1/64
+			min_speed = 1 / 64
 		},
 		match_speed_to_activity = true,
 		match_volume_to_activity = true,
@@ -63,7 +63,7 @@ local custom_properties = {
 		"player-creation",
 		"placeable-off-grid"
 	},
-	icon = SPRITE_PATH.."icon/steam-locomotive.png",
+	icon = SPRITE_PATH .. "icon/steam-locomotive.png",
 	icon_size = 64,
 	icon_mipmaps = 4,
 	pictures = {
@@ -76,7 +76,7 @@ local custom_properties = {
 					lines_per_file = 16,
 					width = 512,
 					height = 512,
-					filename = SPRITE_PATH.."entity/steam-locomotive/body/body.png",
+					filename = SPRITE_PATH .. "entity/steam-locomotive/body/body.png",
 					scale = 0.525,
 					shift = util.by_pixel(0, -19)
 				},
@@ -89,7 +89,7 @@ local custom_properties = {
 					width = 512,
 					height = 512,
 					blend_mode = "additive",
-					filename = SPRITE_PATH.."entity/steam-locomotive/body/mask.png",
+					filename = SPRITE_PATH .. "entity/steam-locomotive/body/mask.png",
 					scale = 0.525,
 					shift = util.by_pixel(0, -19)
 				},
@@ -101,40 +101,12 @@ local custom_properties = {
 					lines_per_file = 16,
 					width = 512,
 					height = 512,
-					filename = SPRITE_PATH.."entity/steam-locomotive/body/shadow.png",
+					filename = SPRITE_PATH .. "entity/steam-locomotive/body/shadow.png",
 					scale = 0.525,
 					shift = util.by_pixel(-7, -19)
 				}
 			}
 		},
-		sloped = {
-			layers = {
-				--base image
-				{
-					direction_count = 128,
-					line_length = 16,
-					lines_per_file = 8,
-					width = 512,
-					height = 512,
-					filename = SPRITE_PATH.."entity/steam-locomotive/sloped/sloped.png",
-					scale = 0.525,
-					shift = util.by_pixel(0, -19)
-				},
-				--color mask
-				{
-					apply_runtime_tint = true,
-					direction_count = 128,
-					line_length = 16,
-					lines_per_file = 8,
-					width = 512,
-					height = 512,
-					blend_mode = "additive",
-					filename = SPRITE_PATH.."entity/steam-locomotive/sloped/mask_sloped.png",
-					scale = 0.525,
-					shift = util.by_pixel(0, -19)
-				}
-			}
-		}
 	},
 	minable = {
 		mining_time = 1,
@@ -152,10 +124,10 @@ local custom_properties = {
 			width = 200,
 			height = 200
 		},
-		shift = {0, -13.5},
+		shift = { 0, -13.5 },
 		size = 2,
 		intensity = 0.6,
-		color = {r = 1, g = 1, b = 1}
+		color = { r = 1, g = 1, b = 1 }
 	},
 	front_light_pictures = {
 		rotated = {
@@ -166,52 +138,85 @@ local custom_properties = {
 			width = 512,
 			height = 512,
 			blend_mode = "normal",
-			filename = SPRITE_PATH.."entity/steam-locomotive/body/lights.png",
+			filename = SPRITE_PATH .. "entity/steam-locomotive/body/lights.png",
 			scale = 0.525,
 			shift = util.by_pixel(0, -19)
 		},
-		sloped = {
-			draw_as_light = true,
-			direction_count = 128,
-			line_length = 16,
-			lines_per_file = 8,
-			width = 512,
-			height = 512,
-			blend_mode = "normal",
-			filename = SPRITE_PATH.."entity/steam-locomotive/sloped/lights_sloped.png",
-			scale = 0.525,
-			shift = util.by_pixel(0, -19)
-		}
 	}
 }
 
-for k,v in pairs(custom_properties) do
+if mods["elevated-rails"] then
+	custom_properties.pictures.sloped = {
+		layers = {
+			--base image
+			{
+				direction_count = 128,
+				line_length = 16,
+				lines_per_file = 8,
+				width = 512,
+				height = 512,
+				filename = SPRITE_PATH .. "entity/steam-locomotive/sloped/sloped.png",
+				scale = 0.525,
+				shift = util.by_pixel(0, -19)
+			},
+			--color mask
+			{
+				apply_runtime_tint = true,
+				direction_count = 128,
+				line_length = 16,
+				lines_per_file = 8,
+				width = 512,
+				height = 512,
+				blend_mode = "additive",
+				filename = SPRITE_PATH .. "entity/steam-locomotive/sloped/mask_sloped.png",
+				scale = 0.525,
+				shift = util.by_pixel(0, -19)
+			}
+		}
+	}
+
+	custom_properties.front_light_pictures.sloped = {
+		draw_as_light = true,
+		direction_count = 128,
+		line_length = 16,
+		lines_per_file = 8,
+		width = 512,
+		height = 512,
+		blend_mode = "normal",
+		filename = SPRITE_PATH .. "entity/steam-locomotive/sloped/lights_sloped.png",
+		scale = 0.525,
+		shift = util.by_pixel(0, -19)
+	}
+end
+
+
+for k, v in pairs(custom_properties) do
 	steam_locomotive[k] = v
 end
 
 steam_locomotive.wheels.rotated.filenames = {
-	SPRITE_PATH.."entity/train-wheel/rotated/train-wheel-1.png",
-	SPRITE_PATH.."entity/train-wheel/rotated/train-wheel-2.png",
-	SPRITE_PATH.."entity/train-wheel/rotated/train-wheel-3.png",
-	SPRITE_PATH.."entity/train-wheel/rotated/train-wheel-4.png",
-	SPRITE_PATH.."entity/train-wheel/rotated/train-wheel-5.png",
-	SPRITE_PATH.."entity/train-wheel/rotated/train-wheel-6.png",
-	SPRITE_PATH.."entity/train-wheel/rotated/train-wheel-7.png",
-	SPRITE_PATH.."entity/train-wheel/rotated/train-wheel-8.png"
+	SPRITE_PATH .. "entity/train-wheel/rotated/train-wheel-1.png",
+	SPRITE_PATH .. "entity/train-wheel/rotated/train-wheel-2.png",
+	SPRITE_PATH .. "entity/train-wheel/rotated/train-wheel-3.png",
+	SPRITE_PATH .. "entity/train-wheel/rotated/train-wheel-4.png",
+	SPRITE_PATH .. "entity/train-wheel/rotated/train-wheel-5.png",
+	SPRITE_PATH .. "entity/train-wheel/rotated/train-wheel-6.png",
+	SPRITE_PATH .. "entity/train-wheel/rotated/train-wheel-7.png",
+	SPRITE_PATH .. "entity/train-wheel/rotated/train-wheel-8.png"
 }
 
-
-steam_locomotive.wheels.sloped.filenames = {
-	SPRITE_PATH.."entity/train-wheel/sloped/train-wheel-sloped-1.png",
-	SPRITE_PATH.."entity/train-wheel/sloped/train-wheel-sloped-2.png",
-	SPRITE_PATH.."entity/train-wheel/sloped/train-wheel-sloped-3.png",
-	SPRITE_PATH.."entity/train-wheel/sloped/train-wheel-sloped-4.png",
-	SPRITE_PATH.."entity/train-wheel/sloped/train-wheel-sloped-5.png",
-	SPRITE_PATH.."entity/train-wheel/sloped/train-wheel-sloped-6.png",
-	SPRITE_PATH.."entity/train-wheel/sloped/train-wheel-sloped-7.png",
-	SPRITE_PATH.."entity/train-wheel/sloped/train-wheel-sloped-8.png"
-}
-
+if mods["elevated-rails"] then
+	steam_locomotive.wheels.sloped.filenames = {
+		SPRITE_PATH .. "entity/train-wheel/sloped/train-wheel-sloped-1.png",
+		SPRITE_PATH .. "entity/train-wheel/sloped/train-wheel-sloped-2.png",
+		SPRITE_PATH .. "entity/train-wheel/sloped/train-wheel-sloped-3.png",
+		SPRITE_PATH .. "entity/train-wheel/sloped/train-wheel-sloped-4.png",
+		SPRITE_PATH .. "entity/train-wheel/sloped/train-wheel-sloped-5.png",
+		SPRITE_PATH .. "entity/train-wheel/sloped/train-wheel-sloped-6.png",
+		SPRITE_PATH .. "entity/train-wheel/sloped/train-wheel-sloped-7.png",
+		SPRITE_PATH .. "entity/train-wheel/sloped/train-wheel-sloped-8.png"
+	}
+end
 local placement_entity = table.deepcopy(steam_locomotive)
 
 placement_entity.name = "rtc-steam-locomotive-placement-entity"
@@ -221,9 +226,10 @@ placement_entity.pictures.rotated = {
 	lines_per_file = 16,
 	width = 512,
 	height = 512,
-	filename = SPRITE_PATH.."entity/steam-locomotive/body/placement_entity.png",
+	filename = SPRITE_PATH .. "entity/steam-locomotive/body/placement_entity.png",
 	scale = 0.525,
 	shift = util.by_pixel(0, -19)
 }
 
-data:extend({custom_smoke, steam_locomotive, placement_entity})
+---@diagnostic disable-next-line: assign-type-mismatch
+data:extend({ custom_smoke, steam_locomotive, placement_entity })
